37a4074a547dba122505d9632f4e0d9f4cb6add7
Paolo Burgio <pburgio@dynamicait.com>
2018-08-02 18:36:06 +0200

    Added Log page

--- IoT-Server/Global.asax.cs
+++ IoT-Server/Global.asax.cs
@@ -11,6 +11,19 @@ namespace IoT_Server
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                HttpContext.Current.Response.AddHeader("Cache-Control", "no-cache");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
+                HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
+                HttpContext.Current.Response.End();
+            }
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
